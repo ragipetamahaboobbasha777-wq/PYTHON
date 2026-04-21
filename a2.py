0e99656fad30a9ae75d4d8382141c8258b5308c9
ragipetamahaboobbasha777-wq <ragipetamahaboobbasha777@gmail.com>
2026-04-21 21:11:46 +0530

    Add files via upload

---
+++ a2.py
@@ -0,0 +1,13 @@
+class A:
+    def display_A(self):
+        print("from class-A")
+class B:
+    def display_B(self):
+        print("from class-B")
+class C(A,B):
+    def display_C(self):
+        print("from class-C")
+S=C()
+S.display_A()
+S.display_B()
+S.display_C()
